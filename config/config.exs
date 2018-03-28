1c5d2e34a102c2ccf98c1e4c4a9bbea283125bc8
At-sushi <dkahw210@kyoto.zaq.ne.jp>
2018-03-28 23:42:58 +0900

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :pleroma, Pleroma.Upload,
 
 # Configures the endpoint
 config :pleroma, Pleroma.Web.Endpoint,
-  url: [host: "localhost"],
+  url: [host: "manaita.arukascloud.io"],
   protocol: "https",
   secret_key_base: "aK4Abxf29xU9TTDKre9coZPUgevcVCFQJe/5xP/7Lt4BEif6idBIbjupVbOrbKxl",
   render_errors: [view: Pleroma.Web.ErrorView, accepts: ~w(json)],
